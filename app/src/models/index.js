fdfdae1f8b12fd44957342b28b62a787eeb0d041
mehdimahjoub <mahjoub.mahdi5@gmail.com>
2021-10-12 18:28:56 +0200

    init test getlist + delete db func

--- app/src/models/index.js
+++ app/src/models/index.js
@@ -34,4 +34,48 @@ db.findCategorys = async () => {
     return await db.product.find().select('category');
 }
 
+// function find product by id
+db.findProductById = async (id) => {
+    return await db.product.findById(id);
+};
+
+// function delete all (use it in test)
+db.deleteAll = async () => {
+    return await db.product.deleteMany();
+}
+
+// function init db with some data
+db._initDb = async () => {
+    const data = [
+        {
+            "name": "my init product 1",
+            "category": "my category test 1",
+            "sku": "skuTest1",
+            "price": 300,
+            "quantity": 10
+        },
+        {
+            "name": "my init product 2",
+            "category": "my category test 2",
+            "sku": "skuTest2",
+            "price": 200,
+            "quantity": 20
+        },
+        {
+            "name": "my init product 3",
+            "category": "my category test 3",
+            "sku": "skuTest3",
+            "price": 100,
+            "quantity": 30
+        }
+    ]
+    let dataDb = await db.product.find();
+    if (!(dataDb && dataDb.length > 0)) {
+        await db.product.create(data);
+    }
+    
+    return data;
+
+}
+
 module.exports = db;
--- app/test/index.test.js
+++ app/test/index.test.js
@@ -4,9 +4,36 @@ const _ = require('lodash');
 const supertest = require('supertest');
 const assert = require('assert').strict;
 const app = require('../src/index');
+const db = require('../src/models');
 
 describe('API', () => {
+    before(async () => {
+        await db.deleteAll();
+    })
+
     //TODO before
+    describe('get list', () => {
+        let products;
+
+        beforeEach(async () => {
+            products = await db._initDb();
+        });
+
+        it('get all products', async () => {
+
+            await supertest(app)
+                .get('/products')
+                .expect(200)
+                .expect(async ({ body }) => {
+
+                    const objTrim = body.map(async (product) =>{
+                       await _.omit(product, ['id','createdAt', 'updatedAt']);
+                    });
+                    assert.deepEqual(await objTrim, products);
+                });
+        })
+    });
+
     describe('create product', () => {
         it('doesn\'t work for with wrong data', async () => {
             await supertest(app)
@@ -52,6 +79,7 @@ describe('API', () => {
                 "quantity": 15
             }
 
+            // add productDoc to db
             await supertest(app)
                 .post('/products')
                 .send(productDoc)
@@ -62,10 +90,36 @@ describe('API', () => {
                             _.omit(body, 'id'),
                             productDoc
                         );
-                    } catch(error) {
+                    } catch (error) {
                         console.log("Error: ", error)
-                    }   
+                    }
                 });
         })
-    })
+    });
+
+    describe('delete', async () => {
+        let element;
+        let id;
+
+        beforeEach(async () => {
+            element = await db.findProducts({ 'name': 'my init product 3' });
+            id = element[0].id;
+        });
+
+        it('doesn\'t work if user is not found', async () => {
+            await supertest(app)
+                .delete('/products/id/606e19f3bbb0134d20dda99b')
+                .expect(404);
+        });
+
+        it('works', async () => {
+            await supertest(app)
+                .delete(`/products/id/${id}`)
+                .expect(204);
+
+            assert(!await db.findProductById(id));
+        });
+    });
+
+
 })
